a02c6926ea2e33273d0e7959f680c40f1dc16d72
NataDent <cherkashyna.n@gmail.com>
2023-11-05 16:46:47 +0200

    Task completed

--- files.js
+++ files.js
@@ -1,7 +1,7 @@
-import fs from 'fs/promises';
-import path from 'path';
-import { validateData } from './helpers/validateData.js';
-import { checkExtention } from './helpers/checkExtention.js';
+import fs from "fs/promises";
+import path from "path";
+import { validateData } from "./helpers/validateData.js";
+import { checkExtention } from "./helpers/checkExtention.js";
 
 export async function createFile(fileName, content) {
   const file = {
@@ -18,11 +18,46 @@ export async function createFile(fileName, content) {
     console.log(`This app doesn't support file with ${extention} extention`);
     return;
   }
-  const filePath = path.resolve('file', fileName);
+  const filePath = path.resolve("file", fileName);
   try {
-    await fs.writeFile(filePath, content, 'utf-8');
-    console.log('File was created successfully');
+    await fs.writeFile(filePath, content, "utf-8");
+    console.log("File was created successfully");
   } catch (error) {
     console.log(error);
   }
 }
+
+export const getFiles = async () => {
+  const folderPath = path.resolve("files");
+  const res = await fs.readdir(folderPath);
+
+  if (!res.length) {
+    console.log("Sorry, there are no files in this folder");
+    return;
+  }
+  res.forEach((el) => console.log(el));
+};
+
+export const getFileInfo = async (fileName) => {
+  const dirPath = path.resolve("files");
+  const files = await fs.readdir(dirPath);
+
+  if (!files.includes(fileName)) {
+    console.log("There are no file with such name");
+    return;
+  }
+  const filePath = path.resolve("files", fileName);
+  const content = await fs.readFile(filePath, "utf-8");
+
+  const { birthtime } = await fs.stat(filePath);
+
+  const ext = path.extname(filePath);
+
+  const nameFile = path.basename(filePath, ext);
+  console.log({
+    content,
+    createdAdd: birthtime,
+    extention: ext,
+    name: nameFile,
+  });
+};
--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-import yargs from 'yargs';
-import { createFile } from './files.js';
+import yargs from "yargs";
+import { createFile, getFiles, getFileInfo } from "./files.js";
+
 const { argv } = yargs(process.argv.slice(2));
 
 function invokeAction({ action, fileName, content }) {
   switch (action) {
-    case 'create':
+    case "create":
       createFile(fileName, content);
       break;
 
-    case '':
+    case "get":
+      getFiles();
       break;
 
-    case '':
+    case "getInfo":
+      getFileInfo(fileName);
       break;
 
     default:
-      console.warn('\x1B[31m Unknown action type!');
+      console.warn("\x1B[31m Unknown action type!");
   }
 }
 
